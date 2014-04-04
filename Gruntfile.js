66447fecc2fe47da22817e84c7c6a6cfb57e4d64
Colin Williams <colin@colincwilliams.com>
2014-04-04 04:29:39 -0600

    Added JavaScript checking and minifying. Added testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
 			src: './_src',
 			config: './_config.yml',
 			layouts: './_src/_layouts',
-			drafts: './_src/_drafts',
+			drafts: './_src/_drafts'
 		},
 		prod: {
 			options: {
@@ -21,6 +21,62 @@ module.exports = function(grunt) {
 				future: true
 			}
 		}
+	},
+	
+	concat: {
+	  options: {
+	    separator: ';'
+	  },
+	  prod: {
+	    src: ['_src/js/**/*.js'],
+	    dest: '_public/js/<%= pkg.name %>.js'
+	  },
+      dev: {
+  	    src: ['_src/js/**/*.js'],
+  	    dest: '_dev/js/<%= pkg.name %>.js'
+      },
+      test: {
+  	    src: ['_src/js/**/*.js'],
+  	    dest: 'libs/latest.js'
+      }
+	},
+	
+	uglify: {
+	  options: {
+	    banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+	  },
+	  prod: {
+	    files: {
+	      '_public/js/<%= pkg.name %>.min.js': ['<%= concat.prod.dest %>']
+	    }
+	  },
+	  dev: {
+	    files: {
+	      '_dev/js/<%= pkg.name %>.min.js': ['<%= concat.dev.dest %>']
+	    }
+	  },
+	  test: {
+	    files: {
+	      'libs/latest.min.js': ['<%= concat.test.dest %>']
+	    }
+	  }
+	},
+	
+	qunit: {
+	  files: ['test/**/*.html']
+	},
+	
+	jshint: {
+	  files: ['Gruntfile.js', '_src/**/*.js', 'test/**/*.js'],
+	  options: {
+	    // options here to override JSHint defaults
+	    globals: {
+	      jQuery: true,
+	      console: true,
+	      module: true,
+	      document: true
+	    }
+	  }
 	},
 
     sass: {
@@ -44,6 +100,15 @@ module.exports = function(grunt) {
 		  files: {
 			  './_dev/css/app.css': './_src/scss/app.scss'
 		  }
+	  },
+	  test: {
+		  options: {
+			  outputStyle: 'nested',
+			  sourceComments: 'normal'
+		  },
+		  files: {
+			  './libs/latest/latest.css': './_src/scss/app.scss'
+		  }
 	  }
     },
 
@@ -57,12 +122,18 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('publish', ['jekyll:prod','sass:prod'])
-  grunt.registerTask('build', ['jekyll:dev','sass:dev'])
+  grunt.registerTask('test', ['jshint','concat:test','uglify:test','sass:test','qunit']);
+  grunt.registerTask('publish', ['test','jekyll:prod','concat:prod','uglify:prod','sass:prod']);
+  grunt.registerTask('build', ['test','jekyll:dev','concat:dev','uglify:dev','sass:dev']);
   grunt.registerTask('serve', ['build','watch']);
   grunt.registerTask('default', ['build']);
-}
+};
---
+++ test/tests.js
@@ -0,0 +1,3 @@
+test("Test!", function(){
+	ok( 1 == "1", "1 == 2, right?!" );
+});
